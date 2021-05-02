4de85cb52442b32471c265d3197165087ab9bbd0
Diana2306 <dnvillanueva86@gmail.com>
2021-05-02 15:09:47 -0500

    Formula 1

---
+++ Taller01/Diana_Villanueva.py
@@ -0,0 +1,10 @@
+message_f1 = """ Pits en formula 1:
+1. Carro llega a pits
+2. Se detiene en el box
+3. El vehiculo tiene 4 llantas
+4. 1er Mecanico se retira llanta vieja
+5. 2do Mecanico coloca llanta nueva
+6. El paso 4 y 5 se repite por c/u de las llantas, es decir, existen 2 mecanicos por llanta
+7. Al terminar el cambio de las llantas el vehiculo sale de pits y se reincorpora a la pista."""
+
+print(message_f1)
