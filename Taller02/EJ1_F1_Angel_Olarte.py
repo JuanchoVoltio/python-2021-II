d97b138532e81374225fd6c1bcd02b66aed99b9b
Angel Olarte <angelolarte5991.15@gmail.com>
2021-05-05 21:15:23 -0500

    primer ejercicio

---
+++ Taller02/EJ1_F1_Angel_Olarte.py
@@ -0,0 +1,19 @@
+'''formula_1'''
+
+proceso = "el vehiculo entre al bloque y los mecanicos se dividen en cuatro grupos, de forma que mietras uno quita una rueda el otro la pone, terminando el cambio de ruedas el vehiculo retoma la carrera"
+
+
+print ("""En una parada de pits existe un vehiculo, bloque respectivo y 8 mecanicos
+                              si quiere saber como opera digite si""")
+input ()
+
+print (proceso)
+
+
+
+
+
+
+
+
+
