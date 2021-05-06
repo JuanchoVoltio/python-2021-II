45d6b50d169c429154733293a083b149fb8abcfd
Angel Olarte <angelolarte5991.15@gmail.com>
2021-05-05 21:48:18 -0500

    Adicion de tiempos aleatorios a primer ejercicio

--- Taller02/EJ1_F1_Angel_Olarte.py
+++ Taller02/EJ1_F1_Angel_Olarte.py
@@ -1,13 +1,31 @@
 '''formula_1'''
 
-proceso = "el vehiculo entre al bloque y los mecanicos se dividen en cuatro grupos, de forma que mietras uno quita una rueda el otro la pone, terminando el cambio de ruedas el vehiculo retoma la carrera"
+import random
 
+Proceso = """El vehiculo entre al bloque y los mecanicos se dividen en cuatro grupos,
+
+de forma que mietras uno quita una rueda el otro la pone,
+
+terminando el cambio de ruedas el vehiculo retoma la carrera"""
+
+Tiempo_cambio_ruedas = random.randint(2,5)
 
 print ("""En una parada de pits existe un vehiculo, bloque respectivo y 8 mecanicos
+
                               si quiere saber como opera digite si""")
 input ()
 
-print (proceso)
+print (Proceso)
+print ()
+
+print ("""Si desea saber el timpo gastado por los mecanicos en cambiar las ruedas
+                                      digite si nuevamente""")
+
+input ()
+
+print ("El tiempo gastado fue de:",Tiempo_cambio_ruedas,"segundos")
+
+
 
 
 
