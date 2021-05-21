e94e482ea7dac0c9abfec951cc66bf46102d8d10
Diana2306 <dnvillanueva86@gmail.com>
2021-05-20 19:38:43 -0500

    Triangulos_V.1.1

--- Taller05/Triangulos_Diana.py
+++ Taller05/Triangulos_Diana.py
@@ -8,11 +8,16 @@ angulo1 = int(input("Digite el primer angulo: "))
 angulo2 = int(input("Digite el segundo angulo: "))
 angulo3 = int(input("Digite el tercer angulo: "))
 
-suma = angulo1 + angulo2 + angulo3
+if angulo1 and angulo2 and angulo3 > 0:
+    suma = angulo1 + angulo2 + angulo3
 
-if suma == 90:
-    print("Es un triángulo rectangulo")
-elif suma < 90:
+if suma == 180:
+    print("Inicia calculo")
+elif angulo1 or angulo2 or angulo3 == 90:
+    print("Es un triángulo rectángulo")
+elif angulo1 or angulo2 or angulo3 < 90:
     print("Es un triángulo acutángulo")
-elif suma > 90:
+elif angulo1 or angulo2 or angulo3 > 90:
     print("Es un triángulo escaleno")
+else:
+    print("Los valores no aplican")
