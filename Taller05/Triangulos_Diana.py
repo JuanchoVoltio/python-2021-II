0517eb2a948dba3ff05585b4de73b57eed6c581b
Diana2306 <dnvillanueva86@gmail.com>
2021-05-20 21:34:10 -0500

    Triángulos Diana

--- Taller05/Triangulos_Diana.py
+++ Taller05/Triangulos_Diana.py
@@ -1,23 +1,22 @@
 """ Ejercicio 1:
-    Dado los 3 angulos de un trangulo definir que tipo de triangulo es.
+    Dado los 3 ángulos de un triángulo definir que tipo de triángulo es.
 """
 
 print("#################### TIPOS DE TRIÁNGULOS ##################")
 
-angulo1 = int(input("Digite el primer angulo: "))
-angulo2 = int(input("Digite el segundo angulo: "))
-angulo3 = int(input("Digite el tercer angulo: "))
+angulo1 = int(input("Digite el primer ángulo: "))
+angulo2 = int(input("Digite el segundo ángulo: "))
+angulo3 = int(input("Digite el tercer ángulo: "))
 
-if angulo1 and angulo2 and angulo3 > 0:
+if angulo1 > 0 and angulo2 > 0 and angulo3 > 0:
     suma = angulo1 + angulo2 + angulo3
-
-if suma == 180:
-    print("Inicia calculo")
-elif angulo1 or angulo2 or angulo3 == 90:
-    print("Es un triángulo rectángulo")
-elif angulo1 or angulo2 or angulo3 < 90:
-    print("Es un triángulo acutángulo")
-elif angulo1 or angulo2 or angulo3 > 90:
-    print("Es un triángulo escaleno")
+    if suma == 180 and angulo1 == 90 or angulo2 == 90 or angulo3 == 90:
+        print("Es un triángulo rectángulo")
+    elif suma == 180 and angulo1 < 90 and angulo2 < 90 and angulo3 < 90:
+        print("Es un triángulo acutángulo")
+    elif suma == 180 and angulo1 > 90 or angulo2 > 90 or angulo3 > 90:
+        print("Es un triángulo escaleno")
+    else:
+        print("Los ángulos no corresponden a un triángulo (La suma de estos debe ser mayor que 180)")
 else:
     print("Los valores no aplican")
