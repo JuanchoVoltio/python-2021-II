d1024b555f2c6b256fff948c5bcec1bf33df648d
Angel Olarte <angelolarte5991.15@gmail.com>
2021-05-24 18:17:40 -0500

    Angel_Olarte

---
+++ Taller03/Operaciones_strings.py
@@ -0,0 +1,37 @@
+#operaciones con string.
+
+print("Uso title")
+
+taller_01= "ejecucion de una parada en pits f1"
+
+taller_01 = taller_01.title()
+print(taller_01)
+print(taller_01.title())
+
+
+
+
+print("Uso upper")
+
+taller_02= "ejecucion de una parada en pits f1"
+
+taller_02 = taller_02.upper()
+print(taller_02)
+print(taller_02.upper())
+
+
+
+
+print("Uso lower")
+
+taller_03= "EJECUCION DE UNA PARAA EN PITS F1"
+
+taller_03 = taller_03.lower()
+print(taller_03)
+print(taller_03.lower())
+
+
+
+
+
+
